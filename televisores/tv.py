12942d16f95dfb455dc3b5b56d0fc5965b1a7d70
gabdevcol <gabrielantonioserrano28@gmail.com>
2023-02-25 13:50:47 -0800

    cambio a metodos de clase en numtv

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,16 +1,20 @@
-class TV:
-    from televisores.control import Control
+from televisores.control import Control
 
-    canal = 1
-    volumen = 1
-    precio = 500
-    control = Control
-    numTV = 0
+class TV:
+
+    canal = int()
+    volumen = int()
+    precio = int()
+    control = Control()
+    numTV = int()
 
     def __init__(self, marca, estado):
         self.marca = marca
         self.estado = estado
-        self.numTV += 1
+        TV.numTV += 1
+        self.canal = 1
+        self.volumen = 1
+        self.precio = 500
 
 
     def setMarca(self, marca):
@@ -32,23 +36,26 @@ class TV:
         return self.precio
 
     def setVolumen(self, volumen):
-        self.volumen = volumen
+        if -1 < volumen < 8 and self.getEstado():
+            self.volumen = volumen
 
     def getVolumen(self):
         return self.volumen
 
     def setCanal(self, canal):
-        if 0 < canal > 121 and self.getEstado():
+        if 0 < canal < 121 and self.getEstado():
             self.canal = canal
 
     def getCanal(self):
         return self.canal
 
-    def setNumTV(self, numTV):
-        self.numTV = numTV
+    @classmethod
+    def setNumTV(cls, num):
+        cls.numTV = num
 
-    def getNumTV(self):
-        return self.numTV
+    @classmethod
+    def getNumTV(cls):
+        return cls.numTV
 
     def turnOn(self):
         self.estado = True
@@ -74,3 +81,57 @@ class TV:
     def volumenDown(self):
         if self.getVolumen() > 0 and self.getEstado():
             self.volumen -= 1
+
+
+if __name__ == "__main__":
+
+    from televisores.tv import TV
+    from televisores.control import Control
+    from televisores.marca import Marca
+
+
+
+    TV.setNumTV(0)
+    marca = Marca("Semsung")
+
+    tv1 = TV(marca, True)
+    tv2 = TV(marca, True)
+    tv3 = TV(marca, True)
+
+    print(TV.getNumTV() == 3)
+
+
+    def testConstructorMarca():
+
+        TV.setNumTV(0)
+
+        marca = Marca("Semsung")
+
+        print(marca.getNombre() == "Semsung")
+
+
+    def testConstructorTV():
+        TV.setNumTV(0)
+        marca = Marca("Xiomi")
+        tv1 = TV(marca, True)
+        ok = False
+
+        if (tv1.getMarca().getNombre() == "Xiomi" and tv1.getEstado()):
+            ok = True
+
+        print(ok)
+
+
+    def testValoresDefecto():
+
+        marca = Marca("Xiomi")
+
+        tv1 = TV(marca, True)
+
+        ok = False
+        if (tv1.getPrecio() == 500 and tv1.getCanal() == 1 and tv1.getVolumen() == 1):
+            ok = True
+        print(ok)
+
+
+    # print(testContador())
